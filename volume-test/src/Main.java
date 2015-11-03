e5f1e82d92b558aada3cbb3ea47e4e2c106410b1
guido <guidouva@users.noreply.github.com>
2015-11-03 10:35:48 +0100

    Test project for comments

---
+++ volume-test/src/Main.java
@@ -0,0 +1,17 @@
+import lib.Factorial;
+
+public class Main {
+	
+	// ignore this line
+	
+	/*
+	 * Ignore all these lines
+	 * 
+	 * Lorem ipsum
+	 */
+
+	public static void main(String[] args) {
+		System.out.println(Integer.toString(Factorial.calculate(10)));
+	}
+
+}
---
+++ volume-test/src/lib/Factorial.java
@@ -0,0 +1,22 @@
+package lib;
+
+/* Calculate the factorial
+ * 
+ * Functions: calculate
+ */
+public class Factorial {
+	
+	// ignore this line
+	public static int calculate(int n) { // don't ignore this line
+		
+		int result = 1;
+		for(int i = 2; i <= n; ++i) { /* do not ignore this line
+			but ignore this line
+		do not ignore this line */ result *= i;
+		}
+		
+		return result;
+
+	}
+
+}
